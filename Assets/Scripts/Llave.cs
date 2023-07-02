491a08c44a6b23cb3b0f9542df1aa8e763335ad5
CruzMatias <117613673+MatiasFCruz@users.noreply.github.com>
2023-07-02 13:23:42 -0300

    sonido de llaves y copia de puerta de puzzles

--- Assets/Scripts/Llave.cs
+++ Assets/Scripts/Llave.cs
@@ -6,6 +6,7 @@ public class Llave : MonoBehaviour
 {
     public Puerta doorToOpen;  // Definición de una variable pública de tipo Puerta llamada doorToOpen
 
+
     private void OnTriggerEnter(Collider other) // Método que se ejecuta cuando un objeto colisiona con este objeto (en este caso, solo se ejecuta al colisionar un objeto con tag "Player")
     {
 
